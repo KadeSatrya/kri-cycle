d70d1fd1919964976eaa3cff5fe25d5a07afdcc9
Kade Satrya <kade.satrya@ui.ac.id>
2024-02-28 10:30:11 +0700

    feat: added route for sampah

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,13 @@ Route::prefix('/kerajinan')->group(function () {
     Route::put('/{id}', [Controllers\KerajinanController::class, 'update'])->name("kerajinan.update");
     Route::delete('/{id}', [Controllers\KerajinanController::class, 'destroy'])->name("kerajinan.destroy");
 });
+
+Route::prefix('/sampah')->group(function () {
+    Route::get('', [Controllers\SampahController::class, 'index'])->name("sampah.index");
+    Route::get('/{id}', [Controllers\SampahController::class, 'show'])->name("sampah.show");
+    Route::get('/create', [Controllers\SampahController::class, 'create'])->name("sampah.create");
+    Route::post('', [Controllers\SampahController::class, 'store'])->name("sampah.store");
+    Route::get('/{id}/edit', [Controllers\SampahController::class, 'update'])->name("sampah.edit");
+    Route::put('/{id}', [Controllers\SampahController::class, 'update'])->name("sampah.update");
+    Route::delete('/{id}', [Controllers\SampahController::class, 'destroy'])->name("sampah.destroy");
+});
